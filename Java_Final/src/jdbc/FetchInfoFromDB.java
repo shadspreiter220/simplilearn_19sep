0f224ca582ba14eb3f78fc4da11b729affe3c535
Shad Spreiter <idaholibaol@ip-172-31-22-19.ec2.internal>
2021-09-25 07:27:39 +0000

    DB Lessons

---
+++ Java_Final/src/jdbc/FetchInfoFromDB.java
@@ -0,0 +1,35 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class FetchInfoFromDB {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		String dbUrl = "jdbc:mysql://localhost:3306/animated_movies";
+		String username = "root";
+		String password = "root";
+		String query = "select * from MOVIES;";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(dbUrl, username, password);
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		
+		while(rs.next()) {
+			System.out.print("Title: " + rs.getString("TITLE") + "\t");
+			System.out.print("Genre: " + rs.getString("GENRE") + "\t");
+			System.out.print("Director" + rs.getString("DIRECTOR") + "\t");
+			System.out.println("Release Year: " + rs.getInt("RELEASE_YEAR"));
+			
+		}
+	
+	}
+	
+}
+
